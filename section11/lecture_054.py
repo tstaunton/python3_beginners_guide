17aaf84f343b6865dff47bcad66fecb1426f7140
Tony Staunton <tony.staunton@gmail.com>
2018-11-03 19:20:29 +0000

    Added source code for section 11 - Input Prompt

---
+++ section11/lecture_054.py
@@ -0,0 +1,7 @@
+
+### Tony Staunton
+### Using the input prompt
+
+prompt = "This is our lecture dicussing Python's input prompt and how to keep it tidy. Please enter your favorite lecture so far and why you like it?"
+
+favorite_lecture = input(prompt)
