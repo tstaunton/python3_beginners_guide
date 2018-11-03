c70aabc5da8ce99730fa9598a0b6e3ab8cb37124
Tony Staunton <tony.staunton@gmail.com>
2018-11-03 14:06:09 +0000

    Added source code for Section 7 - Tuples

---
+++ section7/lecture_031.py
@@ -0,0 +1,16 @@
+### 26 / 10 / 2016
+### Tony Staunton
+### Tuples
+
+coordinates = (1001, 5002)
+print("Original coordinates: ")
+for coordinate in coordinates:
+	print(coordinate)
+
+coordinates = (2002, 7003)
+print("\nNew coordinates: ")
+for coordinate in coordinates:
+	print(coordinate)
+
+
+
